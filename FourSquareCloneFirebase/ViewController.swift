de7ba1baa09db59a33e9d896710a15969f408fc6
Muhammet Midilli <midillimuhammet@gmail.com>
2021-03-08 23:05:13 +0300

    Initial Commit

---
+++ FourSquareCloneFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FourSquareCloneFirebase
+//
+//  Created by Muhammet Midilli on 8.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
